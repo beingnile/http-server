63bbf95e173590db52f50a0794b0058e1c7adc9e
Nile Okomo <okomonile3@gmail.com>
2024-07-04 18:43:36 +0300

    Fix open braces linter errors

--- server.c
+++ server.c
@@ -34,12 +34,14 @@ int main(void)
 	hints.ai_protocol = 0;
 
 	status = getaddrinfo(NULL, PORT, &hints, &res);
-	if (status != 0) {
+	if (status != 0)
+	{
 		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
 		exit(EXIT_FAILURE);
 	}
 
-	for (rp = res; rp != NULL; rp = rp->ai_next) {
+	for (rp = res; rp != NULL; rp = rp->ai_next)
+	{
 		socket_fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
 		if (socket_fd == -1)
 			continue;
@@ -52,21 +54,25 @@ int main(void)
 
 	freeaddrinfo(res);
 
-	if (rp == NULL) {
+	if (rp == NULL)
+	{
 		fprintf(stderr, "[*] Could not bind to socket\n");
 		exit(EXIT_FAILURE);
 	}
 
-	if (listen(socket_fd, BACKLOG) == -1) {
+	if (listen(socket_fd, BACKLOG) == -1)
+	{
 		fprintf(stderr, "[*] Could not listen for connection...\n");
 		exit(EXIT_FAILURE);
 	}
 
 	printf("[*] Connecting to client...\n");
-	while (1) {
+	while (1)
+	{
 		client_addr_len = sizeof(client_addr);
 		sock_fd = accept(socket_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-		if (sock_fd == -1) {
+		if (sock_fd == -1)
+		{
 			fprintf(stderr, "[*] Could not connect to client\n");
 			exit(EXIT_FAILURE);
 		}
@@ -76,7 +82,8 @@ int main(void)
 		msg = "Hello, World!\nInput data to send> ";
 		nsent = send(sock_fd, msg, strlen(msg), MSG_CONFIRM);
 
-		if (nsent == -1) {
+		if (nsent == -1)
+		{
 			fprintf(stderr, "[*] Could not send message to client\n");
 			continue;
 		}
