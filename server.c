5b2a1b38e6f538bc32bdc4a209c4f799870910a9
Nile Okomo <okomonile3@gmail.com>
2024-07-09 11:51:27 +0300

    Add functionality to print client address upon connection

--- server.c
+++ server.c
@@ -1,14 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <unistd.h>
-
-#define PORT "9997"
-#define BUF_SIZE 1024
-#define BACKLOG 10
+#include "server.h"
 
 /**
  * main - Program's entry point
@@ -22,9 +12,6 @@ int main(void)
 	struct addrinfo *res, *rp;
 	struct sockaddr_storage client_addr;
 	socklen_t client_addr_len;
-	ssize_t nread, nsent;
-	char buf[BUF_SIZE];
-	char *msg;
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
@@ -76,26 +63,62 @@ int main(void)
 			exit(EXIT_FAILURE);
 		}
 
-		printf("[*] Connected to client!!!\n");
-
-		msg = "Hello, World!\nInput data to send> ";
-		nsent = send(sock_fd, msg, strlen(msg), MSG_CONFIRM);
-
-		if (nsent == -1)
-		{
-			fprintf(stderr, "[*] Could not send message to client\n");
-			continue;
-		}
-
-		nread = recv(sock_fd, buf, BUF_SIZE, 0);
-
-		printf("Received %lu bytes of data from client\n", nread);
-		printf("Data:\n\n");
-		printf("%s\n", buf);
+		get_peer_addr(&client_addr);
+		handle_client(sock_fd);
 	}
 
 	close(sock_fd);
 	close(socket_fd);
 
 	return (0);
+}
+
+/**
+ * get_peer_addr - Gets the address of the connected peer
+ *
+ * @client_addr: A pointer to struct sockaddr_storage with the client info
+ */
+void get_peer_addr(struct sockaddr_storage *client_addr)
+{
+	char peer_addr[INET6_ADDRSTRLEN];
+	void *addr;
+
+	if (client_addr->ss_family == AF_INET)
+	{
+		struct sockaddr_in *s = (struct sockaddr_in *)client_addr;
+		addr = &(s->sin_addr);
+	}
+	else
+	{
+		struct sockaddr_in6 *s = (struct sockaddr_in6 *)client_addr;
+		addr = &(s->sin6_addr);
+	}
+
+	inet_ntop(client_addr->ss_family, addr, peer_addr, sizeof(peer_addr));
+
+	printf("[*] Connected to client at address: %s\n", peer_addr);
+}
+
+/**
+ * handle_client - Connects to the client using TCP/UDP
+ *
+ * @sock_fd: The file descriptor returned by accept call
+ */
+void handle_client(int sock_fd)
+{
+	ssize_t nread, nsent;
+	char buf[BUF_SIZE];
+	char *msg;
+
+	nread = recv(sock_fd, buf, BUF_SIZE, 0);
+
+	printf("Received %lu bytes of data from client\n", nread);
+	printf("Data:\n\n");
+	printf("%s\n", buf);
+
+	msg = "ACK!\n";
+	nsent = send(sock_fd, msg, strlen(msg), MSG_CONFIRM);
+
+	if (nsent == -1)
+		fprintf(stderr, "[*] Could not send message to client\n");
 }
